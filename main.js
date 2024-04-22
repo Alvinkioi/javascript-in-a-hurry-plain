d4bf3dddd560e13f2330bf276a72e42b97915e62
AlvinKioi <franchisekioi@gmail.com>
2024-04-22 13:47:45 +0300

    added greeting function

--- main.js
+++ main.js
@@ -6,5 +6,20 @@ function clock(params) {
         document.querySelector("span[data-time = seconds]").textContent = localTime.getSeconds().toString().padStart(2,"0");
     }, 1000);
 }
+function greeting(params) {
+    let currentHour = new Date().getHours();
+    let greeting;
 
+    if (currentHour < 12) {
+        greeting = "Good Morning";
+    } else if (currentHour < 18) {
+        greeting = "Good Afternoon";
+    } else {
+        greeting = "Good Evening";
+    }
+    document.querySelector("span[data-time = greeting]").textContent = greeting;
+    
+}
+
+greeting();
 clock();
