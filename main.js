9f081b8a322a75577c9afc8b994c7e7733e64b53
AlvinKioi <franchisekioi@gmail.com>
2024-04-26 13:53:33 +0300

    function for the menu

--- main.js
+++ main.js
@@ -238,7 +238,17 @@ function productsHandler(){
         }
     });
 }
+// Menu function
 
+function menuHandler() {
+    document.querySelector("#open-nav-menu").addEventListener("click", function(){
+        document.querySelector("header nav .wrapper").classList.add("nav-open");
+    });
+    
+    document.querySelector("#close-nav-menu").addEventListener("click", function(){
+        document.querySelector("header nav .wrapper").classList.remove("nav-open");
+    });
+}
 
 greetingHandler();
 clockHandler();
@@ -247,4 +257,5 @@ celsiusToFahr();
 galleryHandler();
 populateProducts();
 productsHandler();
+menuHandler();
 
