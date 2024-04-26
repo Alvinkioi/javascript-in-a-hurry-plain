8189934a1f8198c000cdd22ae1f4ec8e6773d21d
AlvinKioi <franchisekioi@gmail.com>
2024-04-26 13:35:11 +0300

    added weather api key and weather api base url

--- main.js
+++ main.js
@@ -1,25 +1,44 @@
-function clock(params) {
-    setInterval(function() {
-        let localTime = new Date();
-        document.querySelector("span[data-time = hours]").textContent = localTime.getHours().toString().padStart(2,"0");
-        document.querySelector("span[data-time = minutes]").textContent = localTime.getMinutes().toString().padStart(2,"0");
-        document.querySelector("span[data-time = seconds]").textContent = localTime.getSeconds().toString().padStart(2,"0");
-    }, 1000);
-}
-function greeting(params) {
+
+const weatherAPIKey = "f76693de5c51ceb886c717bf7cb2d979";
+const weatherAPIURL = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}&units=metric`;
+
+
+// Greeting Section
+
+function greetingHandler() {
     let currentHour = new Date().getHours();
-    let greeting;
-
-    if (currentHour < 12) {
-        greeting = "Good Morning";
-    } else if (currentHour < 18) {
-        greeting = "Good Afternoon";
-    } else {
-        greeting = "Good Evening";
-    }
-    document.querySelector("span[data-time = greeting]").textContent = greeting;
+    let greetingText;
     
+    if (currentHour < 12) {
+        greetingText = "Good morning!";
+    } else if (currentHour < 19) {
+        greetingText = "Good afternoon!";
+    } else if (currentHour < 24) {
+        greetingText = "Good evening!";
+    } else {
+        greetingText = "Welcome!";
+    }
+    
+    document.querySelector("#greeting").innerHTML = greetingText;
+
+
 }
 
-greeting();
-clock();
+// Clock Section
+function clockHandler() {
+    setInterval(function(){
+        let localTime = new Date();
+        document.querySelector("span[data-time=hours]").textContent = localTime.getHours().toString().padStart(2,"0");
+        document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2,"0");
+        document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2,"0");
+    },1000);
+}
+
+
+
+
+
+
+greetingHandler();
+clockHandler();
+
