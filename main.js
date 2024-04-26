f22b1c8519a92fb97d6c8e407781692174ae70e4
AlvinKioi <franchisekioi@gmail.com>
2024-04-26 13:45:15 +0300

    added functions for the gallery section

--- main.js
+++ main.js
@@ -93,6 +93,37 @@ function celsiusToFahr(temperature){
     return fahr;
 }
 
+// Gallery Section
+
+function galleryHandler() {
+    let mainImage = document.querySelector("#gallery > img");
+    let thumbnails = document.querySelector("#gallery .thumbnails");
+    
+    mainImage.src = galleryImages[0].src;
+    mainImage.alt = galleryImages[0].alt;
+    
+    galleryImages.forEach(function(image, index){
+        let thumb = document.createElement("img");
+        thumb.src = image.src;
+        thumb.alt = image.alt;
+        thumb.dataset.arrayIndex = index;
+        thumb.dataset.selected = index === 0 ? true : false;
+    
+        thumb.addEventListener("click", function(e){
+            let selectedIndex = e.target.dataset.arrayIndex;
+            let selectedImage = galleryImages[selectedIndex];
+            mainImage.src = selectedImage.src;
+            mainImage.alt = selectedImage.alt;
+            thumbnails.querySelectorAll("img").forEach(function(img){
+                img.dataset.selected = false;
+            });
+            e.target.dataset.selected = true;
+    
+        });
+    
+        thumbnails.appendChild(thumb);
+    });
+}
 
 
 
